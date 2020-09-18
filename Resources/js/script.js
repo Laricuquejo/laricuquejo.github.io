f25a75c52251b4707255e27919bbf4b35ca80e8f
Laricuquejo <larissa@cuquejo.org>
2020-09-18 12:48:23 +0200

    My site

---
+++ Resources/js/script.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+  /* For the sticky navigation */
+    $('.js--section-features').waypoint(function(direction) {
+      if (direction == "down") {
+        $('nav').addClass('sticky');
+      } else {
+        $('nav').removeClass('sticky');
+      }
+    }, {
+      offset: '60px'
+    });
+
+  /* Scroll on buttons */
+  $('.js--scroll-to-plans').click(function () {
+    $('html, body').animate({scrollTop: $('.js--section-plans').offset().top}, 1000);
+  });
+  $('.js--scroll-to-calificaciones').click(function () {
+    $('html, body').animate({scrollTop: $('.js--section-plans').offset().top}, 1000);
+  });
+  
+
+
+});
+
